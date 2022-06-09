8b5fc56f98d3b6263b496dea51c67bd9f0c88dc7
Crowdbotics <team@crowdbotics.com>
2022-06-09 12:20:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "pestguard_35651",
+  url: "https://pestguard_35651.botics.co",
+  api: "https://pestguard_35651.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
